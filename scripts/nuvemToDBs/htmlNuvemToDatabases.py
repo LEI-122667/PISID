5eaabe019ef0f0aa9e5b804b4b1ede16e55735d3
122704 <pppns@iscte-iul.pt>
2026-04-28 16:27:10 +0100

    Corrigidos erros MQTT to MySQL e adicionado HTML request

---
+++ scripts/nuvemToDBs/htmlNuvemToDatabases.py
@@ -0,0 +1,135 @@
+import mysql.connector as mariadb
+from mysql.connector import Error
+import pymongo
+import sys
+
+# --- Configuration: MariaDB Local ---
+usermysql, passmysql, hostmysql, database = "root", "root", "localhost", "bd_pisid"
+
+# --- Configuration: MariaDB Cloud ---
+nuvemuser, nuvempass, nuvemhost, nuvemDb = "aluno", "aluno", "194.210.86.10", "maze"
+
+# --- Configuration: MongoDB ---
+uri = "mongodb://root:root@localhost:27017/"
+databaseMongo = "pisid_maze"
+collection_setup_name = "setup"
+collection_corredores_name = "corredores"
+
+# --- 1. Capture Arguments from PHP ---
+# Expected order: IDSimulacao, outliers_temp, outliers_som, alerta_temp_h, alerta_temp_l, alerta_som, time_fechar, ruido_limite
+try:
+    php_vars = {
+        "id_sim": sys.argv[1],
+        "out_temp": sys.argv[2],
+        "out_som": sys.argv[3],
+        "al_temp_h": sys.argv[4],
+        "al_temp_l": sys.argv[5],
+        "al_som": sys.argv[6],
+        "t_fechar": sys.argv[7],
+        "ruido_lim": sys.argv[8]
+    }
+except IndexError:
+    print("Error: Missing arguments from PHP. Script requires 8 arguments.")
+    sys.exit(1)
+
+try:
+    # --- 2. Establish Connections ---
+
+    # MariaDB Local
+    conn_local = mariadb.connect(host=hostmysql, user=usermysql, passwd=passmysql, db=database, autocommit=True)
+    cursor_local = conn_local.cursor()
+    print("Connected to Local MariaDB.")
+
+    # MariaDB Cloud
+    conn_nuvem = mariadb.connect(host=nuvemhost, user=nuvemuser, passwd=nuvempass, db=nuvemDb, autocommit=True)
+    cursor_nuvem = conn_nuvem.cursor(dictionary=True)
+    print("Connected to Cloud MariaDB.")
+
+    # MongoDB
+    client_mongo = pymongo.MongoClient(uri, serverSelectionTimeoutMS=2000)
+    client_mongo.server_info()  # Test connection
+    db_mongo = client_mongo[databaseMongo]
+    col_setup = db_mongo[collection_setup_name]
+    col_corredores = db_mongo[collection_corredores_name]
+    print("Connected to MongoDB.")
+
+    # --- 3. Process SetupMaze ---
+    print("\nSyncing SetupMaze...")
+    cursor_nuvem.execute("SELECT * FROM SetupMaze")
+    records = cursor_nuvem.fetchall()
+
+    # Reset MongoDB collection
+    col_setup.drop()
+
+    for row in records:
+        # A. Insert into Local MariaDB
+        sql_mariadb = """INSERT INTO SetupMaze (NumberRooms, NumberMarsamis, NumberPlayers, NormalTemperature,
+                                                TemperatureVarHighToleration, TemperatureVarLowToleration, NormalNoise, \
+                                                NoiseVarToleration)
+                         VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
+
+        cursor_local.execute(sql_mariadb, (
+            row['numberrooms'], row['numbermarsamis'], row['numberplayers'],
+            row['normaltemperature'], row['temperaturevarhightoleration'],
+            row['temperaturevarlowtoleration'], row['normalnoise'], row['noisevartoleration']
+        ))
+
+        # B. Insert into MongoDB (Adding the extra PHP arguments)
+        setup_doc = {
+            "numbermarsamis": row['numbermarsamis'],
+            "numberrooms": row['numberrooms'],
+            "numberplayers": row['numberplayers'],
+            "normalnoise": float(row['normalnoise']),
+            "noisevartoleration": float(row['noisevartoleration']),
+            "normaltemperature": row['normaltemperature'],
+            "temperaturevarhightoleration": row['temperaturevarhightoleration'],
+            "temperaturevarlowtoleration": row['temperaturevarlowtoleration'],
+            # Adding the arguments from PHP
+            "outliers_temperatura": float(php_vars["out_temp"]),
+            "outliers_som": float(php_vars["out_som"]),
+            "IDSimulacao": int(php_vars["id_sim"])
+        }
+        col_setup.insert_one(setup_doc)
+
+    # --- 4. Process Corridors ---
+    print("Syncing Corridors...")
+    cursor_nuvem.execute("SELECT Rooma, Roomb FROM Corridor")
+    corredores = cursor_nuvem.fetchall()
+
+    col_corredores.drop()
+
+    for i, row in enumerate(corredores, start=1):
+        # A. MariaDB Local
+        cursor_local.execute("INSERT INTO Corridor (RoomA, RoomB) VALUES (%s, %s)", (row['Rooma'], row['Roomb']))
+
+        # B. MongoDB
+        corredor_doc = {
+            "idCorredor": i,
+            "origin": row['Rooma'],
+            "destination": row['Roomb']
+        }
+        col_corredores.insert_one(corredor_doc)
+
+    # --- 5. Insert ConfigJogo (MariaDB Only) ---
+    print("Inserting ConfigJogo...")
+    sql_config = """INSERT INTO ConfigJogo
+                    (IDSimulacao, outliers_temperatura, outliers_som, alerta_temperatura_high,
+                     alerta_temperatura_low, alerta_som, time_fecharcorredores, ruidolimite_fecharcorredores)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
+
+    config_vals = (
+        php_vars["id_sim"], php_vars["out_temp"], php_vars["out_som"],
+        php_vars["al_temp_h"], php_vars["al_temp_l"], php_vars["al_som"],
+        php_vars["t_fechar"], php_vars["ruido_lim"]
+    )
+    cursor_local.execute(sql_config, config_vals)
+
+    print("\nAll systems synchronized successfully.")
+
+except Exception as e:
+    print(f"\nFATAL ERROR: {e}")
+
+finally:
+    if 'conn_local' in locals(): conn_local.close()
+    if 'conn_nuvem' in locals(): conn_nuvem.close()
+    if 'client_mongo' in locals(): client_mongo.close()
